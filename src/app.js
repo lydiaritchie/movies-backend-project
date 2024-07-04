f8f3d683afa5d3480cc8da612ac4dc0b7d474d93
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-07-04 17:20:32 +0700

    Created controller, router and service files for movies, reviews, theaters

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
+
+
 
 module.exports = app;
