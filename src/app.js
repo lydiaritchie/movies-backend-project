eb0553df745c5c0d11559ef1be11b6a65abaaa1d
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-07-05 18:05:13 +0700

    delete /reviews/reviewId works

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
-//app.use("/reviews", reviewsRouter);
+app.use("/reviews", reviewsRouter);
 //app.use("/theaters", theatersRouter);
 
 app.use(notFound);
--- src/utils/shared.service.js
+++ src/utils/shared.service.js
@@ -24,7 +24,6 @@ function listCritics(){
 }
 
 
-
 module.exports = {
     listMovies, 
     listTheaters,
